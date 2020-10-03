78abb195a1d6ab471ef8d387a513bda4a478ade1
RichJ1129 <richjoseph1129@gmail.com>
2020-10-03 11:30:13 -0400

    Creation of user model and routes. Creatioon of auth folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,13 @@ import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { HomeModule} from './home/home.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReactiveFormsModule,
-    FormsModule,
-    LoginComponent,
-    RegisterComponent,
-    UserProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
