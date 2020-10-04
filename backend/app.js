eb3ea94668474eea235b93e852be5901a34efef8
RichJ1129 <richjoseph1129@gmail.com>
2020-10-04 00:53:30 -0400

    Main.yml changed for integrated deploy

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/", express.static(path.join(__dirname, "angular-bullmarket")));
-
+app.use(express.static(path.join(__dirname, 'angular-bullmarket')));
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -36,8 +35,8 @@ app.use("/", express.static(path.join(__dirname, "angular-bullmarket")));
 // });
 
 app.use("/api/user", userRoutes);
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "angular-bullmarket", "index.html"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'angular-bullmarket/index.html'));
 });
 
 module.exports = app;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
