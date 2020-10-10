2effdfe2e4f7a33ec78b7a0dca4544a40505dfb7
AustinEngineering23 <43730301+AustinEngineering23@users.noreply.github.com>
2020-10-10 11:40:40 -0700

    Test Table

--- src/app/commodities/commodities.component.ts
+++ src/app/commodities/commodities.component.ts
@@ -7,25 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommoditiesComponent implements OnInit {
 
-  constructor() { }
+  headers: any;
+  rows: any;
 
-  headers = ['test', 'pleaseWork'];
-  rows =  [{
-    ID : '1',
-    Name : 'Rahul',
-    Age : '21',
-    Gender : 'Male',
-    Country : 'India'
-  },
-    {
-      ID : '2',
-      Name : 'Ajay',
-      Age : '25',
+  constructor() {
+    this.headers = ['test', 'pleaseWork'];
+
+    this.rows =  [{
+      ID : '1',
+      Name : 'Rahul',
+      Age : '21',
       Gender : 'Male',
       Country : 'India'
-    }];
+    },
+      {
+        ID : '2',
+        Name : 'Ajay',
+        Age : '25',
+        Gender : 'Male',
+        Country : 'India'
+      }];
+  }
+
 
   ngOnInit(): void {
   }
 
 }
+
+
