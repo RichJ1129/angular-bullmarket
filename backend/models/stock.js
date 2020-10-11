934d66f1c09b09c2b1e8fa8ee5a46849a03d989b
RichJ1129 <richjoseph1129@gmail.com>
2020-10-11 01:48:07 -0400

    Stock Post route created

--- backend/models/stock.js
+++ backend/models/stock.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const StockSchema = mongoose.Schema({
-  name: {
+  stockName: {
     type: String,
     required: true
   },
--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -4,6 +4,29 @@ const Stock = require("../models/stock");
 
 const router = express.Router();
 
+router.post("/stocks", (req, res, next) => {
+  const stock = new Stock({
+    stockName: req.body.stockName,
+    symbol: req.body.symbol,
+    price: req.body.price,
+    marketCap: req.body.marketCap
+  });
+  stock.save().then(
+    result => {
+      res.status(201).json({
+        message: "Stock Created!",
+        result: result
+        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+
 
 
 
