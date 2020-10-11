7276fade59695f6d25b583b21075d08af72ede60
RichJ1129 <richjoseph1129@gmail.com>
2020-10-11 01:20:19 -0400

    New files created: 1. get-stock.js: Script that will get stocks each day and then post to the route. 2. stock.js: route that will post to database each day.

---
+++ backend/routes/stock.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const Stock = require("../models/stock");
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
