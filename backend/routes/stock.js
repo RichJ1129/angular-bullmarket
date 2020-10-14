1cbc09bfbd60cbc05424cfd947a31422b83df4c9
RichJ1129 <richjoseph1129@gmail.com>
2020-10-13 23:55:40 -0400

    API get for stocks

--- backend/routes/stock.js
+++ backend/routes/stock.js
@@ -2,7 +2,30 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const Stock = require("../models/stock");
 const router = express.Router();
-const axios = require('axios');
+
+router.get("", (req, res, next) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const stockQuery = Stock.find(
+    { "Date"}
+  );
+  let fetchedStocks;
+  if (pageSize && currentPage) {
+    stockQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  stockQuery
+    .then(documents => {
+      fetchedStocks = documents;
+      return Stock.count();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Stocks fetched successfully!",
+        stocks: fetchedStocks,
+        maxStocks: count
+      });
+    });
+});
 
 
 module.exports = router;
