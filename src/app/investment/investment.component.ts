7280ccfad9997fcc40ac2058539d9f7061fb45f5
oregonstatetm <35415571+oregonstatetm@users.noreply.github.com>
2020-10-24 13:38:46 -0700

    Buttons and Number Input
    Add Buttons for Buying, Selling, Number Input. Add additional investment data to page.

--- src/app/investment/investment.component.ts
+++ src/app/investment/investment.component.ts
@@ -25,6 +25,46 @@ const INVESTMENT_DATA: Investment[] = [
     currentPrice: 190,
     purchasePrice: 183.42,
     net: -65.80
+  },
+  {
+    name: 'OIL',
+    symbol: 'USO',
+    shares: 25,
+    currentPrice: 27.88,
+    purchasePrice: 29.07,
+    net: -29.75
+  },
+  {
+    name: 'MICROSOFT',
+    symbol: 'MSFT',
+    shares: 30,
+    currentPrice: 218.55,
+    purchasePrice: 188.00,
+    net: +916.50
+  },
+  {
+    name: 'NOKIA',
+    symbol: 'NOK',
+    shares: 200,
+    currentPrice: 4.06,
+    purchasePrice: 2.50,
+    net: +312
+  },
+  {
+    name: 'COSTA RICA RURAL',
+    symbol: 'RRCOS',
+    shares: 1,
+    currentPrice: 32400,
+    purchasePrice: 32500,
+    net: +100
+  },
+  {
+    name: 'EUROS',
+    symbol: 'EURUSD',
+    shares: 10000,
+    currentPrice: 1.17,
+    purchasePrice: 1.10,
+    net: +700
   }
 ]; 
 @Component({
@@ -35,6 +75,12 @@ const INVESTMENT_DATA: Investment[] = [
 export class InvestmentComponent {
 
   dataSource = INVESTMENT_DATA;
-  displayedColumns: string[] = ['name', 'symbol', 'shares', 'currentPrice', 'purchasePrice', 'net'];
+  displayedColumns: string[] = ['name', 'symbol', 'shares', 'currentPrice', 'purchasePrice', 'net', 'buy', 'sell', 'number'];
+
+  onClickMe(){
+    console.log("Yolo");
+
+  }
+
 };
 
