ee3ceb506f278879fd2c6ac6aadbc3d3d549db2f
oregonstatetm <35415571+oregonstatetm@users.noreply.github.com>
2020-11-07 22:37:19 -0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,7 @@ const investmentRoutes = require("./routes/investment");
 const app = express();
 mongoose
   .connect(
-    // process.env.MONGO_ATLAS //Replace with mongo database url if running locally
-    'mongodb+srv://josephri:TempPass@cluster0.murwd.mongodb.net/bull_market?retryWrites=true&w=majority'
+    process.env.MONGO_ATLAS //Replace with mongo database url if running locally
   )
   .then(() => {
     console.log("Connected to database!");
