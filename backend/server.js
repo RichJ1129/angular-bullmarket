cb8e2f5b27a15f49a816ffb906e4ae42294f78b1
Rich Joseph <josephri@oregonstate.edu>
2020-11-11 17:12:18 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ crontab.scheduleJob("30 08 * * 5" , function(){
   timezone: "America/New_York"
 });
 
-crontab.scheduleJob("00 17 * * 0-5" , function(){
+crontab.scheduleJob("30 17 * * 0-5" , function(){
   currencies.getCurrency();
 },{
   schedule: true,
