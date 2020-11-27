34b01715e87aac36969565b7eff0854b082c1a3c
Richard Joseph <richjoseph1129@gmail.com>
2020-11-27 03:05:41 -0500

    - Allows user to click map to bring to country page. - Bosnia fixed.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,10 @@
 import {OnInit} from '@angular/core';
 import {ViewChild, Component} from '@angular/core';
 import {DxVectorMapComponent} from 'devextreme-angular';
+import { DxVectorMapModule, DxSelectBoxModule, DxTextBoxModule } from 'devextreme-angular';
+import { DxDataGridComponent } from 'devextreme-angular';
+import { Router } from '@angular/router';
+
 
 import * as mapsData from 'devextreme/dist/js/vectormap-data/world.js';
 import {Service} from './home.service';
@@ -10,23 +14,39 @@ import {ProfileService} from '../profile/profile.service';
   selector: 'app-home',
   providers: [Service],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 
 export class HomeComponent implements OnInit {
   worldMap: any = mapsData.world;
   gdp: Object;
   animalDecider;
+  selectedCountry: string;
+  @ViewChild(DxVectorMapComponent, { static: false }) vectorMap: DxVectorMapComponent;
 
-  constructor(service: Service, private profileService: ProfileService) {
+
+  constructor(service: Service,
+              private profileService: ProfileService,
+              private router: Router) {
     this.gdp = service.getGDP();
     this.customizeLayers = this.customizeLayers.bind(this);
     this.profileService.getAnimal()
       .subscribe(val => {this.animalDecider = val;
-      })
+      });
   }
 
-  customizeTooltip(arg) {
+  onClick(e) {
+    console.log(e);
+    console.log(this.vectorMap.instance.convertToGeo(e.event.x, e.event.y));
+    const target = e.target;
+
+    if (target && this.gdp[target.attribute('name')] && target.attribute('name') !== 'Greenland') {
+      this.router.navigate(['/country/' + target.attribute('name')]);
+    }
+  }
+
+  customizeTooltip(arg): { text: string } {
+    // console.log(arg.attribute('name'));
     if (arg.attribute('gdp')) {
       return {
         text: arg.attribute('name') + ': ' + arg.attribute('gdp') / 1000 + 'B GDP'
@@ -34,7 +54,7 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  customizeLayers(elements) {
+  customizeLayers(elements): void {
     elements.forEach((element) => {
       element.attribute('gdp', this.gdp[element.attribute('name')]);
     });
@@ -54,7 +74,6 @@ export class HomeComponent implements OnInit {
 
   // constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
