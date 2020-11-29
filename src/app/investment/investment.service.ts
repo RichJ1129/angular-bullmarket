659d6465fff24b2ded5a179fe83574dc528e7a07
Richard Joseph <richjoseph1129@gmail.com>
2020-11-29 15:43:08 -0500

    - Fixed whitespace

--- src/app/investment/investment.service.ts
+++ src/app/investment/investment.service.ts
@@ -19,7 +19,7 @@ interface IDAuthData{
   _id: string;
   email: string;
   userName: string;
-};
+}
 
 interface InvestmentMath{
   name: string;
@@ -28,7 +28,7 @@ interface InvestmentMath{
   shares: number;
   currentPrice: number;
   transactionPrice: number;
-};
+}
 
 const backendURL = environment.apiURL;
 
@@ -36,14 +36,15 @@ const backendURL = environment.apiURL;
 export class InvestmentService {
 
   private investments: Investment[] = [];
-  constructor(private http: HttpClient, private router: Router, private authService: AuthService,private stockApi: StockService, private countryApi: RealEstateService, private currencyApi: CurrencyService, private commodityApi: CommodityService) {}
+  // tslint:disable-next-line:max-line-length
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService, private stockApi: StockService, private countryApi: RealEstateService, private currencyApi: CurrencyService, private commodityApi: CommodityService) {}
   private investmentsUpdated = new Subject<Investment[]>();
   private investmentsUpdated2 = new Subject<InvestmentMath[]>();
   private userEmail: string;
   private UID: string;
   private queryUID: string;
   private result: any = [];
-  private currencyBalance=0;
+  private currencyBalance = 0;
   private returnValue: number;
   private investmentValue: number;
   private portfolio: InvestmentMath[] = [];
@@ -66,18 +67,18 @@ getUserID(){
 
   public transactionHistory(UID: string) {
     let params = new HttpParams();
-    params = params.append('userID',UID);
-    return this.http.get(backendURL + '/investment', {params: params});
+    params = params.append('userID', UID);
+    return this.http.get(backendURL + '/investment', {params});
   }
 
-  public async numberOfShares(UID: string, symbol: string) : Promise<number> {
+  public async numberOfShares(UID: string, symbol: string): Promise<number> {
     this.tempshares = 0;
     let params = new HttpParams();
-    params = params.append('userID',UID);
-    this.result = await this.http.get(backendURL + '/investment', {params: params}).toPromise();
+    params = params.append('userID', UID);
+    this.result = await this.http.get(backendURL + '/investment', {params}).toPromise();
 
-    for(let x=0;x<this.result.length;x++){ 
-      if(this.result[x].symbol == symbol)
+    for (let x = 0; x < this.result.length; x++){
+      if (this.result[x].symbol == symbol)
       {
         this.tempshares += +(this.result[x].shares);
       }
@@ -85,108 +86,108 @@ getUserID(){
     return this.tempshares;
   }
 
-  public async getInvestments(UID: string) : Promise<InvestmentMath[]> {
-    //Retrieve Investment List
+  public async getInvestments(UID: string): Promise<InvestmentMath[]> {
+    // Retrieve Investment List
     let params = new HttpParams();
-    params = params.append('userID',UID);
-    this.result = await this.http.get(backendURL + '/investment', {params: params}).toPromise();
-    
-   //Create Net Investment Portfolio from Investment List
-   var temp2:InvestmentMath;
-   for(let x=0;x<this.result.length;x++){
-   this.add=1; // Reset Check for each Result Investment
-   temp2={ 
-     "currentPrice":this.result[x].transactionPrice, 
-     "name":this.result[x].name, 
-     "symbol":this.result[x].symbol,
-     "shares":this.result[x].shares,
-     "type":this.result[x].assetType, 
-     "transactionPrice":this.result[x].transactionPrice
+    params = params.append('userID', UID);
+    this.result = await this.http.get(backendURL + '/investment', {params}).toPromise();
+
+   // Create Net Investment Portfolio from Investment List
+    let temp2: InvestmentMath;
+    for (let x = 0; x < this.result.length; x++){
+   this.add = 1; // Reset Check for each Result Investment
+   temp2 = {
+     currentPrice: this.result[x].transactionPrice,
+     name: this.result[x].name,
+     symbol: this.result[x].symbol,
+     shares: this.result[x].shares,
+     type: this.result[x].assetType,
+     transactionPrice: this.result[x].transactionPrice
    };
-   for(let y=0;y<this.portfolio.length;y++){ //Look to see if this has been add
-     if(this.portfolio[y].symbol==temp2.symbol){
+   for (let y = 0; y < this.portfolio.length; y++){ // Look to see if this has been add
+     if (this.portfolio[y].symbol == temp2.symbol){
        this.portfolio[y].shares = (+this.portfolio[y].shares + +temp2.shares); // Add or Subtract Shares from this Symbol
-       this.add=0; //Jump to Next Investment
-       y=+(this.portfolio.length);// Symbols Match, Exit 'y' For Loop
+       this.add = 0; // Jump to Next Investment
+       y = +(this.portfolio.length); // Symbols Match, Exit 'y' For Loop
      }
    }
-   if(this.add==1){ //Investment Doesn't Exist in Portfolio Array, Add it
+   if (this.add == 1){ // Investment Doesn't Exist in Portfolio Array, Add it
      this.portfolio.push(temp2);
    }
  }
 
- console.group("Current Prices:");
- //Update Current Price for Each Investment
- for(let z=0;z<this.portfolio.length;z++)
+    console.group('Current Prices:');
+ // Update Current Price for Each Investment
+    for (let z = 0; z < this.portfolio.length; z++)
  {
-   if(this.portfolio[z].type =="Stock" || this.portfolio[z].type=="stock"){
-     //Retrieve Stock Information
+   if (this.portfolio[z].type == 'Stock' || this.portfolio[z].type == 'stock'){
+     // Retrieve Stock Information
      this.stockApi.getOneStock(this.portfolio[z].symbol).subscribe(stockData2 => {
        this.stock2 = { stockName: stockData2.stockName, symbol: stockData2.symbol, price: stockData2.price, marketCap: stockData2.marketCap, closeDate: stockData2.closeDate, pERatio: stockData2.pERatio };
 
-       this.portfolio[z].currentPrice=this.stock2.price[0];
-     })
+       this.portfolio[z].currentPrice = this.stock2.price[0];
+     });
    }
-   if(this.portfolio[z].type =="Bond" || this.portfolio[z].type=="bond"){
-    //No change
+   if (this.portfolio[z].type == 'Bond' || this.portfolio[z].type == 'bond'){
+    // No change
    }
-   if(this.portfolio[z].type =="Commodities" || this.portfolio[z].type=="commodities"){
+   if (this.portfolio[z].type == 'Commodities' || this.portfolio[z].type == 'commodities'){
 
-   //Retrieve Commodity Current Price
+   // Retrieve Commodity Current Price
    this.commodityApi.getOneCommodity(this.portfolio[z].symbol).subscribe(commodityData => {
-     this.commodity = {commodityName: commodityData.commodityName, symbol: commodityData.symbol, etfPrice: commodityData.etfPrice, commodityUnit: "", closeDate: [] };
-   
-     this.portfolio[z].currentPrice=this.commodity.etfPrice[0];
+     this.commodity = {commodityName: commodityData.commodityName, symbol: commodityData.symbol, etfPrice: commodityData.etfPrice, commodityUnit: '', closeDate: [] };
+
+     this.portfolio[z].currentPrice = this.commodity.etfPrice[0];
    });
 
 
    }
-   if(this.portfolio[z].type =="Real Estate" || this.portfolio[z].type=="realestate"){
-      //No change
+   if (this.portfolio[z].type == 'Real Estate' || this.portfolio[z].type == 'realestate'){
+      // No change
    }
-   if(this.portfolio[z].type =="Currency" || this.portfolio[z].type=="currency"){
+   if (this.portfolio[z].type == 'Currency' || this.portfolio[z].type == 'currency'){
 
-   //Retrieve Currency Current Price
+   // Retrieve Currency Current Price
    this.currencyApi.getOneCurrency(this.portfolio[z].symbol).subscribe(currencyData => {
      this.currency = { currencyName: currencyData.currencyName, ticker: currencyData.ticker, rates: currencyData.rates, timeStamp: currencyData.timeStamp};
 
-     this.portfolio[z].currentPrice=this.currency.rates[0];
-   })
+     this.portfolio[z].currentPrice = this.currency.rates[0];
+   });
    }
  }
 
- //Return Portfolio Object
- return this.portfolio;
+ // Return Portfolio Object
+    return this.portfolio;
  }
 
   public async getInvestmentValue(UID: string) {
     let params = new HttpParams();
-    params = params.append('userID',UID);
+    params = params.append('userID', UID);
 
-    this.result = await this.http.get(backendURL + '/investment', {params: params}).toPromise();
+    this.result = await this.http.get(backendURL + '/investment', {params}).toPromise();
     this.investmentValue = 0;
 
-    for (let x=0; x<this.result.length; x++){
+    for (let x = 0; x < this.result.length; x++){
       this.investmentValue += (this.result[x].transactionPrice * this.result[x].shares);
     }
 
-    this.returnValue = (+(Math.round(this.investmentValue * 100) / 100).toFixed(2))
+    this.returnValue = (+(Math.round(this.investmentValue * 100) / 100).toFixed(2));
     return this.returnValue;
 
   }
 
   public async getCurrencyBalance(UID: string, baseCurrency: string) {
     let params = new HttpParams();
-    params = params.append('userID',UID);
-    params = params.append('baseCurrency',baseCurrency);
-  
-    this.result = await this.http.get(backendURL + '/baseCurrency', {params: params}).toPromise();
+    params = params.append('userID', UID);
+    params = params.append('baseCurrency', baseCurrency);
+
+    this.result = await this.http.get(backendURL + '/baseCurrency', {params}).toPromise();
     this.currencyBalance = 0;
-    for(let y=0; y<this.result.length; y++){
-       this.currencyBalance+=(+this.result[y].shares);
+    for (let y = 0; y < this.result.length; y++){
+       this.currencyBalance += (+this.result[y].shares);
     }
 
-    this.returnValue = (+(Math.round(this.currencyBalance * 100) / 100).toFixed(2))
+    this.returnValue = (+(Math.round(this.currencyBalance * 100) / 100).toFixed(2));
     return this.returnValue;
   }
 
