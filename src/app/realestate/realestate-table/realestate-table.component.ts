47d1ecd5471e29e614b142cbd03d74611cfa9458
Richard Joseph <richjoseph1129@gmail.com>
2020-11-28 23:43:02 -0500

    - Pagination complete

--- src/app/realestate/realestate-table/realestate-table.component.ts
+++ src/app/realestate/realestate-table/realestate-table.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import { Country } from '../country.model';
 import { RealEstateService } from '../realestate.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import {MatPaginator} from "@angular/material/paginator";
 
 
 
@@ -11,33 +12,33 @@ import { MatSort } from '@angular/material/sort';
   templateUrl: './realestate-table.component.html',
   styleUrls: ['./realestate-table.component.css']
 })
-export class RealEstateTableComponent implements OnInit {
+export class RealEstateTableComponent implements AfterViewInit {
 
   displayedColumns: any[] = ['countryName', 'urbanRent', 'urbanPrice', 'ruralRent', 'ruralPrice'];
   RealEstateData: any = [];
   dataSource;
   //dataSource=REALESTATE_DATA;//MatTableDataSource<Country>;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
   constructor(private realestateApi: RealEstateService) {
     this.realestateApi.getRealEstate().subscribe(data => {
-      console.log("RE FROM DB");
-      console.log(data);
+      // console.log("RE FROM DB");
+      // console.log(data);
       this.RealEstateData = data;
 
       for(var x=0; x<this.RealEstateData.length; x++){
-        this.RealEstateData[x].ruralPrice = (12*(this.RealEstateData[x].ruralPE * this.RealEstateData[x].ruralRent)).toFixed(2);
-        this.RealEstateData[x].urbanPrice = (12*(this.RealEstateData[x].urbanPE * this.RealEstateData[x].urbanRent)).toFixed(2);
+        this.RealEstateData[x].ruralPrice = (12 * (this.RealEstateData[x].ruralPE * this.RealEstateData[x].ruralRent)).toFixed(2);
+        this.RealEstateData[x].urbanPrice = (12 * (this.RealEstateData[x].urbanPE * this.RealEstateData[x].urbanRent)).toFixed(2);
       }
 
-      this.dataSource= new MatTableDataSource<Country>(this.RealEstateData);
-    })
+      this.dataSource = new MatTableDataSource<Country>(this.RealEstateData);
+      this.dataSource.paginator = this.paginator;
+    });
   }
 
-
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
   }
 
 }
--- src/app/stocks/stock-table/stock-table.component.ts
+++ src/app/stocks/stock-table/stock-table.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Stock } from '../stock.model';
 import { StockService } from '../stock.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-stock-table',
   templateUrl: './stock-table.component.html',
   styleUrls: ['./stock-table.component.css']
 })
-export class StockTableComponent implements OnInit {
+export class StockTableComponent implements AfterViewInit {
 
   displayedColumns: any[] = ['stockName', 'symbol', 'price', 'pERatio', 'marketCap'];
   StockData: any = [];
   dataSource: MatTableDataSource<Stock>;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
 
   constructor(private stockApi: StockService) {
@@ -22,9 +25,10 @@ export class StockTableComponent implements OnInit {
       this.StockData = data;
       this.dataSource = new MatTableDataSource<Stock>(this.StockData);
       this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
     });
   }
 
-  ngOnInit(): void { }
-
+  ngAfterViewInit(): void {
+  }
 }
